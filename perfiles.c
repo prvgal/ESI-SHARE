ab1c07826310fb53eccb732e95e7b612e92c756a
Pablo Rivero <88436787+prvgal@users.noreply.github.com>
2023-03-13 14:22:00 +0100

    Update perfiles.c

--- perfiles.c
+++ perfiles.c
@@ -20,11 +20,55 @@ void ReservarPerfil(tPerfiles *infoper){
 }
 
 void RegistrarPerfil(tPerfiles *infoper){
+    unsigned int numPerfiles = LongitudVectorEstructuras(infoper) + 1, id[ID];
+
     ReservarPerfil(infoper);
 
-    unsigned int numPerfiles = LongitudVectorEstructuras(infoper);
+    GenerarID(infoper, infoper[numPerfiles].Id_usuario);
 
+    printf("\nNombre de usuario: ");
+    fflush(stdin);
+    fgets(infoper[numPerfiles].Nomb_usuario, MAX_N, stdin);
+    EliminarSaltoLinea(infoper[numPerfiles].Nomb_usuario);
+
+    printf("\n%s, indique el nombre de la localidad en la que resida: ", infoper[numPerfiles].Nomb_usuario);
+    fflush(stdin);
+    fgets(infoper[numPerfiles].Localidad, MAX_L, stdin);
+    EliminarSaltoLinea(infoper[numPerfiles].Localidad);
+
+    infoper[numPerfiles].Perfil_usuario = False;
+
+    printf("\n%s, indique el nombre de usuario para acceder al sistema: ", infoper[numPerfiles].Nomb_usuario);
+    fflush(stdin);
+    fgets(infoper[numPerfiles].Usuario, MAX_U, stdin);
+    EliminarSaltoLinea(infoper[numPerfiles].Usuario);
+
+    printf("\nContrasena para %s: ", infoper[numPerfiles].Usuario);
+    fflush(stdin);
+    fgets(infoper[numPerfiles].Contrasena, MAX_C, stdin);
+    EliminarSaltoLinea(infoper[numPerfiles].Contrasena);
+}
+
+void ModificarCampo(tPerfiles *infoper){
+    int op;
     
+
+    puts("Que campo desea modificar? ");
+
+    do{
+        printf("\n<1> Nombre de usuario. <2> Localidad. <3> Usuario de acceso. <4> Contrasena.");
+        scanf("%i", &op);
+
+        if(op < 1 || op > 4)
+            puts("Por favor, introduzca uno de los campos mencionados.");
+    } while(op < 1 || op > 4);
+
+    switch(op){
+        case 1: CambiarNombre(); break;
+        case 2: CambiarLocalidad(); break;
+        case 3: CambiarUsuarioAcceso(); break;
+        case 4: CambiarContrasena(); break;
+    }
 }
 
 // void LeerFicheroEnEstructura(tPerfiles *infoper, unsigned int numPerfiles)
@@ -32,17 +76,18 @@ void RegistrarPerfil(tPerfiles *infoper){
 
 /* Funciones NO exportables */
 
-static void GenerarID(tPerfiles *infoper, char id[ID]){
-    unsigned int ID_usuario = LongitudVectorEstructuras(infoper) + 1;
-    int i = ID-1, temp1 = ID_usuario, temp2 = ID_usuario;     // temp1 contendrá la parte decimal y temp2 la parte entera
+static void GenerarID(tPerfiles *infoper, unsigned int id[ID]){
+    int i = ID-1, aux1 = LongitudVectorEstructuras(infoper) + 1, aux2 = aux1;     // aux1 contendrá la parte decimal y aux2 la parte entera
     
     for(; i >= 0; i--){
-        temp2 %= 10;
-        id[i] = temp2;
-        temp1 = (unsigned int)floor(temp1/10);
-        temp2 = temp1;
+        aux2 %= 10;
+        id[i] = aux2;
+        aux1 = (unsigned int)floor(aux1/10);  // Aproximación a la baja, es decir, si el numero es 3,8, en aux1 se guradará 3
+        aux2 = aux1;
     } 
 }
+
+// static ¿ void ? ObtenerID(){}
 
 static unsigned int LongitudVectorEstructuras(tPerfiles *infoper){
     // Sabemos que el fichero Usuarios.txt tendrá tantas líneas como la longitud del vector de estructuras infoper.
@@ -62,32 +107,10 @@ static unsigned int LongitudVectorEstructuras(tPerfiles *infoper){
         i++;
     }
 
-    rewind(fich);
+    // Falta eliminar la última linea que se crea al ver las lineas del fichero
+
+    rewind(fich); 
     fclose(fich);
-
-    return i;
-}
-
-/*static unsigned int LongitudVectorEstructuras(tPerfiles *infoper){
-    unsigned int tamTotal, tamPrimerElem, longVec;
-
-    tamTotal = sizeof(infoper);             // Numero de bytes que ocupa el vector.
-    printf("%u\n", tamTotal);
-    tamPrimerElem = sizeof(tPerfiles);     // Numero de bytes que ocupa el primer elemento del vector.
-    printf("%u\n", tamPrimerElem);
-    longVec = tamTotal/tamPrimerElem;       // Longitud del vector.
-    printf("%u", longVec);
-
-    return longVec;
-}*/
-
-static unsigned int NumeroDigitos(unsigned int num){
-    unsigned int i = 0;
-
-    while(num > 0){
-        num /= 10;
-        i++;
-    }
 
     return i;
 }
