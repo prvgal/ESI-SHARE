c351dbdd034ae48c95c91edff1a9a84dc8fa0fd1
Jose039 <126725552+Jose039@users.noreply.github.com>
2023-03-19 19:54:51 +0100

    Update Viajes.h

--- Viajes.h
+++ Viajes.h
@@ -4,11 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "tipos.h"
 
 #define MATRICULA 8
 #define HORA 6
 #define TIPO 2
+
+typedef struct{
+    logico abierto;
+    logico cerrado;
+    logico iniciado;
+    logico finalizado;
+    logico anulado;
+} estado_viajes;
+
+typedef struct{
+    int ID;                         //6 dígitos
+    char matricula [MATRICULA];     //Matrícula de vehícula usado para el viaje
+    char fecha[11];                 //Formato dd/mm/aa
+    char hora_inicio [HORA];        //Formato 24h - Mínimo 06:00
+    char hora_llegada [HORA];       //Formato 24h - Máximo 22:30
+    int Nplazas;                    //Número de plazas disponibles
+    char tipo [TIPO];               //I - Ida o V - Vuelta
+    float importe;                  //Entre 0 y 15 € por persona
+    estado_viajes estado;           //Solo un estado activo al mismo tiempo
+} viajes;
 
 //FUNCIONES PRIVADAS:
 
