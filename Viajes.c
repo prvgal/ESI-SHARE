d4daffc5920528ac853fa88e35eea13c66f309d3
Jose039 <126725552+Jose039@users.noreply.github.com>
2023-03-19 20:18:35 +0100

    Update Viajes.c

--- Viajes.c
+++ Viajes.c
@@ -2,79 +2,43 @@
             /*Módulo de viaje: se guarda la ID única del viaje, la matrícula del vehículo, fecha y hora de inicio y llegada,
                         plazas libres, tipo de viaje, el importe y estado del viaje en el fichero Viajes.txt*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define MATRICULA 8
-#define HORA 6
-#define TIPO 2
-
-//Declaración de funciones
-void generar_ID_viaje();
-void introducir_fecha(int *hoy);
-void horas(int hoy);
-void plazas();
-
-typedef struct{         //0 = Falso, 1 = Verdadero
-    int abierto;
-    int cerrado;
-    int iniciado;
-    int finalizado;
-    int anulado;
-} estado_viajes;
-
-typedef struct{
-    int ID;  //6 dígitos
-    char matricula [MATRICULA];
-    char fecha[11];
-    char hora_inicio [HORA];   //Mínimo 06:00
-    char hora_llegada [HORA];  //Máximo 22:30
-    int Nplazas;     //Actualiza número de plazas
-    char tipo [TIPO];  //I(Ida) o V(Vuelta)
-    float importe;  //Entre 0 y 15 € por persona
-    estado_viajes estado;  //Solo un estado activo al mismo tiempo
-} viajes;
-
 viajes *viaje;
-int num = 0;
+int posUsuario = 0;
 
 int main(){     //Main temporal para probar que funciona correctamente
-    int hoy;
+    logico hoy;
 
-    if (num==0){
+    if (posUsuario==0){
         viaje = (viajes*)malloc(sizeof(viajes));
-        num = 1;
+        posUsuario = 1;
     }
     else
-        viaje = (viajes*)realloc(viaje, (num+1)*sizeof(viajes));
+        viaje = (viajes*)realloc(viaje, (posUsuario+1)*sizeof(viajes));
 
-    generar_ID_viaje();
+    generar_ID_viaje(viaje, posUsuario);
 
-    introducir_fecha(&hoy);
+    introducir_fecha(viaje, posUsuario, &hoy);
 
-    horas(hoy);
+    horas(viaje, posUsuario, hoy);
 
-    viaje[num].Nplazas = 4;
+    viaje[posUsuario].Nplazas = 4;
 
-    plazas();
+    plazas(viaje, posUsuario);
 
     free(viaje);
     return 0;
 }
 
-//Precondición: El usuario habrá elegido la opción de realizar un viaje
-//Postcondición: Se le asigna una ID de 6 dígitos al viaje
-
-void generar_ID_viaje(){
+void generar_ID_viaje(viajes *viaje, int posUsuario){
     int i, p = 0;
     do
     {
         srand(time(NULL));   //Inicializa la semilla del generador de números aleatorios
-        viaje[num].ID = (rand() % (999999 - 100000 + 1)) + 100000;   //Genera un número aleatorio entre 0 y 999999
+        viaje[posUsuario].ID = (rand() % (999999 - 100000 + 1)) + 100000;   //Genera un número aleatorio entre 0 y 999999
 
-        for(i=0; i < num && p == 0; i++)    //Compara la ID generada con las otras existentes, si es igual a otra se genera una nueva
+        for(i=0; i < posUsuario && p == 0; i++)    //Compara la ID generada con las otras existentes, si es igual a otra se genera una nueva
     {
-        if(viaje[num].ID == viaje[i].ID){
+        if(viaje[posUsuario].ID == viaje[i].ID){
             p = 1;
             printf("Hubo un error generando la ID del viaje.");
             printf("Generando una nueva ID...");
@@ -82,14 +46,12 @@ void generar_ID_viaje(){
     }
     } while (p==1);
     printf("ID del viaje generada correctamente.\n");
-    printf("ID del viaje = %i\n", viaje[0].ID);
+    printf("ID del viaje = %i\n", viaje[posUsuario].ID);
 }
 
-//Precondición: El usuario habrá elegido la opción de realizar un viaje
-//Postcondición: El usuario habrá introducido una fecha del viaje válida
-void introducir_fecha(int *hoy){
+void introducir_fecha(viajes *viaje, int posUsuario, logico *hoy){
     int dia, mes, ano, dias_en_mes, fecha_valida = 0;   //Indica si la fecha introducida es válida
-    *hoy = 0;
+    *hoy = False;
 
     time_t tiempo_actual;
     struct tm *fecha_actual;
@@ -106,7 +68,7 @@ void introducir_fecha(int *hoy){
         fecha_actual = localtime(&tiempo_actual);
 
         if(dia == fecha_actual->tm_mday && mes == fecha_actual->tm_mon + 1 && ano == fecha_actual->tm_year + 1900){
-            *hoy = 1;
+            *hoy = True;
         }
 
         if (ano < fecha_actual->tm_year + 1900 || (ano == fecha_actual->tm_year + 1900 && mes < fecha_actual->tm_mon + 1) || (ano == fecha_actual->tm_year + 1900 && mes == fecha_actual->tm_mon + 1 && dia < fecha_actual->tm_mday)){
@@ -133,20 +95,18 @@ void introducir_fecha(int *hoy){
             continue;
         }
 
-        sprintf(viaje[num].fecha, "%02d/%02d/%04d", dia, mes, ano);  //Convierte la fecha a una cadena
+        sprintf(viaje[posUsuario].fecha, "%02d/%02d/%04d", dia, mes, ano);  //Convierte la fecha a una cadena
         fecha_valida = 1;
     } while (!fecha_valida);
 
-    printf("La fecha ingresada es: %s\n", viaje[num].fecha);
+    printf("La fecha introducida es: %s\n", viaje[posUsuario].fecha);
 }
 
-//Precondición: El usuario habrá elegido iniciar un viaje
-//Postcondición: El usuario habrá introducido una hora de inicio y llegada válidas
-void horas(int hoy){
+void horas(viajes *viaje, int posUsuario, logico hoy){
     char entrada[6];
     int estado = 0, minutos_llegada, minutos_inicio, minutos_actual;
 
-    time_t hora_actual = time(NULL);    //Obtiene la hora actual
+    time_t hora_actual = time(NULL);                    //Obtiene la hora actual
     struct tm* hora_local = localtime(&hora_actual);    //Convierte a hora local
     
     minutos_actual = hora_local->tm_hour * 60 + hora_local->tm_min; //Convierte la hora local a minutos
@@ -157,8 +117,8 @@ void horas(int hoy){
         scanf("%5s", entrada);
         int horas, minutos;
         minutos_inicio = atoi(entrada+3) + atoi(entrada) * 60;
-        if (sscanf(entrada, "%d:%d", &horas, &minutos) == 2 && minutos_inicio >= (6 * 60) && minutos_inicio <= (22 * 60 + 25) && minutos >= 0 && minutos < 60 && (hoy!=1 || minutos_inicio > minutos_actual)){ //Comprueba que la hora de inicio introducida es válida
-            sprintf(viaje[num].hora_inicio, "%02d:%02d", horas, minutos);
+        if (sscanf(entrada, "%d:%d", &horas, &minutos) == 2 && minutos_inicio >= (6 * 60) && minutos_inicio <= (22 * 60 + 25) && minutos >= 0 && minutos < 60 && (hoy!=True || minutos_inicio > minutos_actual)){ //Comprueba que la hora de inicio introducida es válida
+            sprintf(viaje[posUsuario].hora_inicio, "%02d:%02d", horas, minutos);
             estado = 1;
         } else{
             printf("La hora de inicio debe estar entre las 06:00 y las 22:25 y debe ser anterior a la actual si el viaje es hoy. Introduzca la hora de nuevo.\n");
@@ -174,7 +134,7 @@ void horas(int hoy){
         int horas, minutos;
         minutos_llegada = atoi(entrada+3) + atoi(entrada) * 60;
         if (sscanf(entrada, "%d:%d", &horas, &minutos) == 2 && minutos_llegada >= (6 * 60) && minutos >= 0 && minutos < 60 && minutos_llegada <= (22 * 60 + 30)){   //Comprueba que la hora de llegada introducida es válida
-            sprintf(viaje[num].hora_llegada, "%02d:%02d", horas, minutos);
+            sprintf(viaje[posUsuario].hora_llegada, "%02d:%02d", horas, minutos);
             if (minutos_llegada >= minutos_inicio + 5) {
                 estado = 1;
             } else{
@@ -185,17 +145,15 @@ void horas(int hoy){
         }
     }
 
-    printf("La hora de inicio es %s\n", viaje[num].hora_inicio);
-    printf("La hora de llegada es %s\n", viaje[num].hora_llegada);
+    printf("La hora de inicio es %s\n", viaje[posUsuario].hora_inicio);
+    printf("La hora de llegada es %s\n", viaje[posUsuario].hora_llegada);
 }
 
-//Precondición: El usuario habrá elegido unirse a un viaje
-//Postcondición: Actualiza el número de plazas libres de un viaje
-void plazas(){
-    if(viaje[num].Nplazas != 0){
-        viaje[num].Nplazas -= 1;
-        printf("Quedan %i plazas libres en el viaje %i", viaje[num].Nplazas, viaje[num].ID);
+void plazas(viajes *viaje, int posUsuario){
+    if(viaje[posUsuario].Nplazas != 0){
+        viaje[posUsuario].Nplazas -= 1;
+        printf("Quedan %i plazas libres en el viaje %i", viaje[posUsuario].Nplazas, viaje[posUsuario].ID);
     }
     else
-    printf("No quedan plazas en el viaje %i", viaje[num].ID);
+    printf("No quedan plazas en el viaje %i", viaje[posUsuario].ID);
 }
