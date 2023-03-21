2e49bfed7f388c07395cc524d3730973d58b4fbb
JUwUanma <126818557+JUwUanma@users.noreply.github.com>
2023-03-21 17:42:08 +0100

    Add files via upload

--- Trayecto.c
+++ Trayecto.c
@@ -4,17 +4,17 @@
 
 
 void gestionar_trayecto(viajes viaje){
-    int e;
+    int eleccion;
     do{
         void eleccion_trayecto();
-        scanf("%i",&e);
-        switch(e){
+        scanf("%i",&eleccion);
+        switch(eleccion){
             case 1: paradas_trayecto(viaje);
             case 2: modificar_trayecto(viaje);
             case 3: borrar_trayecto(viaje);
             case 0: break;
         }
-    }while(e!=0);
+    }while(eleccion!=0);
 }
 
 
@@ -22,10 +22,10 @@ void gestionar_trayecto(viajes viaje){
 void paradas_trayecto(viajes viaje){
 char c,poblacion[21];
 FILE* pasos;
-   pasos = fopen("Pasos.txt", "w");
+    pasos = fopen("Pasos.txt", "w");
 
     do{
-        printf("Introduce una de las poblaciones de paso: \n");
+        printf("Introduzca una de las poblaciones de paso: \n");
         fflush(stdin);
         fgets(poblacion, 21, stdin);
         if (pasos != NULL) {
@@ -36,19 +36,47 @@ FILE* pasos;
         printf("¿Quiere añadir una nueva población? (s/n) \n");
         fflush(stdin);
         scanf("%c",c);
-}while(c=='S'||c=='s');
+     }while(c=='S'||c=='s');
 }
 
+
+
 void eleccion_trayecto(){
-    printf("Elija una opcion: \n   1. Agregar paradas \n   2. Modificar paradas \n   3. Borrar paradas \n   0. Cancelar \n")
+    printf("\n Elija una opcion: \n   1. Agregar paradas \n   2. Modificar paradas \n   3. Borrar paradas \n   0. Cancelar \n");
 }
 
 
 
 void modificar_trayecto(viajes viaje){
+    borrar_trayecto(viaje);
+    paradas_trayecto(viaje);
 }
 
 
 
 void borrar_trayecto(viajes viaje){
+
+    FILE *pasos, *pasosaux;
+    char ID[6], linea_borrar[100], linea[100];
+    int i=0;
+    pasos = fopen("Pasos.txt", "r");
+    pasosaux = fopen("PasosAux.txt", "w");
+
+    while (fgets(ID, 6, pasos)!=NULL) {
+        i++;
+        if (ID == viaje.ID) {
+            fgets(linea, 100, pasos);
+            strcpy(linea_borrar, linea);
+        } else {
+            fputs(linea, pasosaux);
+        }
+    }
+
+    fclose(pasos);
+    fclose(pasosaux);
+
+    remove("Pasos.txt");
+    rename("PasosAux.txt", "Pasos.txt");
+
+    printf("Paradas borradas correctamene \n");
 }
--- Trayecto.h
+++ Trayecto.h
@@ -1,8 +1,35 @@
 #ifndef TRAYECTO_H_INCLUDED
 #define TRAYECTO_H_INCLUDED
 
+
+
 //precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero)
 //postcondición: Crea un fichero llamado Pasos.txt donde guarda la ID de un viaje junto con sus respectivas paradas
-void paradas(viajes);
+void paradas_trayecto(viajes);
+
+
+
+//precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero)
+//postcondición: Permite la elección entre: Agregar paradas, Modificar paradas, Borrar paradas o Cancelar
+void gestionar_trayecto(viajes);
+
+
+
+//precondición: Ninguna
+//postcondición: Despliega un menú sin funcionalidad, meramente visual e ilustrativo
+void eleccion_trayecto();
+
+
+//precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero)
+//postcondición: Permite al usuario modificar las paradas de su trayecto en caso de equivocación
+void modificar_trayecto(viajes viaje);
+
+
+
+//precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero)
+//postcondición: Permite al usuario borrar su registro de trayecto del fichero Pasos.txt
+void borrar_trayecto(viajes viaje);
+
+
 
 #endif // TRAYECTO_H_INCLUDED
