f9d97117a19750ff94e1b2c24001efdd5fcc548b
JUwUanma <126818557+JUwUanma@users.noreply.github.com>
2023-03-21 12:32:09 +0100

    Add files via upload

--- Trayecto.c
+++ Trayecto.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include "viajes.h"
 
-void paradas(viajes viaje){
+
+
+void gestionar_trayecto(viajes viaje){
+    int e;
+    do{
+        void eleccion_trayecto();
+        scanf("%i",&e);
+        switch(e){
+            case 1: paradas_trayecto(viaje);
+            case 2: modificar_trayecto(viaje);
+            case 3: borrar_trayecto(viaje);
+            case 0: break;
+        }
+    }while(e!=0);
+}
+
+
+
+void paradas_trayecto(viajes viaje){
 char c,poblacion[21];
 FILE* pasos;
    pasos = fopen("Pasos.txt", "w");
@@ -19,4 +37,18 @@ FILE* pasos;
         fflush(stdin);
         scanf("%c",c);
 }while(c=='S'||c=='s');
+}
+
+void eleccion_trayecto(){
+    printf("Elija una opcion: \n   1. Agregar paradas \n   2. Modificar paradas \n   3. Borrar paradas \n   0. Cancelar \n")
+}
+
+
+
+void modificar_trayecto(viajes viaje){
+}
+
+
+
+void borrar_trayecto(viajes viaje){
 }
