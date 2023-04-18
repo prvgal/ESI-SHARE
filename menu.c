1d34eae65a37e1f07c86b15aad80fd8b4fc870d6
Pablo Rivero <88436787+prvgal@users.noreply.github.com>
2023-04-18 11:03:32 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
+#include "Viajes.c"
 #include "perfiles.c"
 
 int main(){
-      
+	int pos;
+    tPerfil *info;
 
-    
+    info = CrearListaDePerfiles();
+    CargarPerfiles(info);
+
+    pos = Inicio(info);
+
+    Menu(info, pos);
+
+    free(info);
     return 0;
 }
