f22bd9df68beb381c6609ea8d0aff386035177f6
Jose039 <126725552+Jose039@users.noreply.github.com>
2023-04-19 14:56:50 +0200

    Update Trayecto.h

--- Trayecto.h
+++ Trayecto.h
@@ -6,7 +6,7 @@
 
 //precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero) y una localidad para el usuario en concreto (esta Localidad una cadena de 20 caracteres)
 //postcondición: Permite la elección entre: Agregar paradas, Modificar paradas, Borrar paradas, listar viajes comunes al usuario o Cancelar
-void gestionar_trayecto(viajes, tPerfil);
+void gestionar_trayecto(char idt [7], tPerfil *usuario);
 
 
 
@@ -22,37 +22,37 @@ void mostrar_paradas(char[]);
 //********************************************************************************************************
 
 
-void inicio_trayecto(viajes);
+void inicio_trayecto(char idt [7]);
 
-static void final_ESI(viajes);
+static void final_ESI(char idt [7]);
 
-static void ida_ESI_cadiz(viajes);
+static void ida_ESI_cadiz(char idt [7]);
 
-static void ida_ESI_sanfer(viajes);
+static void ida_ESI_sanfer(char idt [7]);
 
-static void ida_ESI_sanfer_cadiz(viajes);
+static void ida_ESI_sanfer_cadiz(char idt [7]);
 
-static void ida_ESI_sanfer_cadiz_fin(viajes);
+static void ida_ESI_sanfer_cadiz_fin(char idt [7]);
 
-static void ida_ESI_jerez(viajes);
+static void ida_ESI_jerez(char idt [7]);
 
-static void ida_ESI_jerez_puerto(viajes);
+static void ida_ESI_jerez_puerto(char idt [7]);
 
-static void ida_ESI_puerto(viajes);
+static void ida_ESI_puerto(char idt [7]);
 
-static void inicio_ESI(viajes);
+static void inicio_ESI(char idt [7]);
 
-static void ESI_cadiz(viajes);
+static void ESI_cadiz(char idt [7]);
 
-static void ESI_sanfernando(viajes);
+static void ESI_sanfernando(char idt [7]);
 
-static void ESI_sanfer_puertor(viajes);
+static void ESI_sanfer_puertor(char idt [7]);
 
-static void ESI_jerez(viajes);
+static void ESI_jerez(char idt [7]);
 
-static void ESI_jerez_puertor(viajes);
+static void ESI_jerez_puertor(char idt [7]);
 
-static void ESI_puertostamaria(viajes);
+static void ESI_puertostamaria(char idt [7]);
 
 
 //********************************************************************************************************
@@ -65,57 +65,57 @@ static void ESI_puertostamaria(viajes);
 //********************************************************************************************************
 
 
-static void ida_ESI_cadiz_puertor(viajes);
+static void ida_ESI_cadiz_puertor(char idt [7]);
 
-static void ida_ESI_cadiz_fin(viajes);
+static void ida_ESI_cadiz_fin(char idt [7]);
 
-static void ida_ESI_sanfer_cadiz_puertor(viajes);
+static void ida_ESI_sanfer_cadiz_puertor(char idt [7]);
 
-static void ida_ESI_sanfer_cadiz_fin(viajes);
+static void ida_ESI_sanfer_cadiz_fin(char idt [7]);
 
-static void ida_ESI_sanfer_puertor(viajes);
+static void ida_ESI_sanfer_puertor(char idt [7]);
 
-static void ida_ESI_sanfer_fin(viajes);
+static void ida_ESI_sanfer_fin(char idt [7]);
 
-static void ida_ESI_jerez_puerto_puertor_fin(viajes);
+static void ida_ESI_jerez_puerto_puertor_fin(char idt [7]);
 
-static void ida_ESI_jerez_puertor_fin(viajes);
+static void ida_ESI_jerez_puertor_fin(char idt [7]);
 
-static void ida_ESI_jerez_puertor(viajes);
+static void ida_ESI_jerez_puertor(char idt [7]);
 
-static void ida_ESI_jerez_fin(viajes);
+static void ida_ESI_jerez_fin(char idt [7]);
 
-static void ida_ESI_puerto_puertor_fin(viajes);
+static void ida_ESI_puerto_puertor_fin(char idt [7]);
 
-static void ida_ESI_puerto_fin(viajes);
+static void ida_ESI_puerto_fin(char idt [7]);
 
-static void ida_ESI_puertor(viajes);
+static void ida_ESI_puertor(char idt [7]);
 
-static void ESI_cadiz_puertor(viajes);
+static void ESI_cadiz_puertor(char idt [7]);
 
-static void ESI_cadiz_fin(viajes);
+static void ESI_cadiz_fin(char idt [7]);
 
-static void ESI_puertoreal(viajes);
+static void ESI_puertoreal(char idt [7]);
 
-static void ESI_sanfer_cadiz(viajes);
+static void ESI_sanfer_cadiz(char idt [7]);
 
-static void ESI_sanfer_cadiz_puerto(viajes);
+static void ESI_sanfer_cadiz_puerto(char idt [7]);
 
-static void ESI_sanfer_puertor_fin(viajes);
+static void ESI_sanfer_puertor_fin(char idt [7]);
 
-static void ESI_sanfer_fin(viajes);
+static void ESI_sanfer_fin(char idt [7]);
 
-static void ESI_jerez_puertor_puerto_fin(viajes);
+static void ESI_jerez_puertor_puerto_fin(char idt [7]);
 
-static void ESI_jerez_puertor_fin(viajes);
+static void ESI_jerez_puertor_fin(char idt [7]);
 
-static void ESI_jerez_puerto(viajes);
+static void ESI_jerez_puerto(char idt [7]);
 
-static void ESI_jerez_fin(viajes);
+static void ESI_jerez_fin(char idt [7]);
 
-static void ESI_puerto_puertor_fin(viajes);
+static void ESI_puerto_puertor_fin(char idt [7]);
 
-static void ESI_puerto_fin(viajes);
+static void ESI_puerto_fin(char idt [7]);
 
 
 //********************************************************************************************************
@@ -124,13 +124,13 @@ static void ESI_puerto_fin(viajes);
 
 //precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero)
 //postcondición: Permite al usuario modificar las paradas de su trayecto en caso de equivocación
-static void modificar_trayecto(viajes);
+static void modificar_trayecto(char idt [7]);
 
 
 
 //precondición: Debe haberse generado una ID para un viaje (esta ID sería de tipo entero)
 //postcondición: Permite al usuario borrar su registro de trayecto del fichero Pasos.txt
-static void borrar_trayecto(viajes);
+static void borrar_trayecto(char idt [7]);
 
 
 
