05e8cdd5ea758ef62d54a786d0b1230ac50a7637
Pablo Rivero <88436787+prvgal@users.noreply.github.com>
2023-04-19 15:00:15 +0200

    Update perfiles.h

--- perfiles.h
+++ perfiles.h
@@ -13,7 +13,7 @@
 typedef struct {
     char Id_usuario[ID];        // 4 digitos.
     char Nomb_usuario[MAX_N];   // 20 caracteres mas el \0. Nombre completo del usuario
-    char Localidad[MAX_L];      // 20 caracteres mas el \0. Ubicaci�n de donde sale el usuario con su veh�culo compartido.
+    char Localidad[MAX_L];      // 20 caracteres mas el \0. Ubicación de donde sale el usuario con su vehículo compartido.
     char Perfil_usuario[MAX_PU];    // 13 caracteres (administrador) incluyendo el \0. Verifica si el usuario es administrador o de tipo usuario
     char Usuario[MAX_U];        // 5 caracteres mas el \0. Nombre de usuario para acceder al sistema.
     char Contrasena[MAX_C];     // 8 caracteres mas el \0. 
@@ -22,131 +22,131 @@ typedef struct {
 
 /* FUNCIONES PUBLICAS */
 
-// Precondici�n: Recibe la direcci�n de memoria donde se encuentra la primera posici�n del vector de estructuras de tipo tPerfil debe estar inicializada con antelaci�n.
-// Poscondici�n: Devuelve la posici�n donde se encuentra el usuario en el vector de estructuras de tipo tPerfil
+// Precondición: Recibe la dirección de memoria donde se encuentra la primera posición del vector de estructuras de tipo tPerfil debe estar inicializada con antelación.
+// Poscondición: Devuelve la posición donde se encuentra el usuario en el vector de estructuras de tipo tPerfil
 int Inicio(tPerfil *infoper);
 
-// Precondici�n: Debe haberse iniciado sesi�n o registrado con antelaci�n. Recibe la direcci�n de memoria del primer elemento del vector de estructuras tipo tPerfil
-//               y un entero que se corresponde con la posici�n del usuario que inicio sesi�n o se registr� con antelaci�n
-// Poscondici�n: Dependiendo del tipo de perfil que tenga el usuario, desembocar� a un men� u otro, en caso de ser administrador, se llama a MenuAdmin, en caso de no serlo, se llama a MenuUser
+// Precondición: Debe haberse iniciado sesión o registrado con antelación. Recibe la dirección de memoria del primer elemento del vector de estructuras tipo tPerfil
+//               y un entero que se corresponde con la posición del usuario que inicio sesión o se registró con antelación
+// Poscondición: Dependiendo del tipo de perfil que tenga el usuario, desembocará a un menú u otro, en caso de ser administrador, se llama a MenuAdmin, en caso de no serlo, se llama a MenuUser
 void Menu(tPerfil *infoper, int posUsua);
 
-// Precondici�n: 
-// Poscondici�n: Devuelve un vector de tipo tPerfil del tama�o equivalente al numero de usuarios guardados en Usuarios.txt
+// Precondición: 
+// Poscondición: Devuelve un vector de tipo tPerfil del tamaño equivalente al numero de usuarios guardados en Usuarios.txt
 tPerfil *CrearListaDePerfiles(void);
 
-// Precondici�n: Recibe un vector de estructuras tipo tPerfil, el cual, deber�a tener un tama�o m�nimo, en caso de no tenerlo, llama a SignUp para crear un nuevo usuario.
-// Poscondici�n: carga toda la informaci�n de Usuarios.txt en el vector de estructuras de tipo tPefil.
+// Precondición: Recibe un vector de estructuras tipo tPerfil, el cual, deberá tener un tamaño mínimo, en caso de no tenerlo, llama a SignUp para crear un nuevo usuario.
+// Poscondición: carga toda la información de Usuarios.txt en el vector de estructuras de tipo tPefil.
 void CargarPerfiles(tPerfil *infoper);
 
-// Precondici�n: recibe una cadena de caraacteres y dos enteros, uno para el numero que queremos realizar la ID y el numero de d�gitos de esa ID.
-// Poscondici�n: guarda en id una cadena de caracteres, por ejemplo, si num = 12 y numDigitos = 4, en id se encontrar� 0012
+// Precondición: recibe una cadena de caraacteres y dos enteros, uno para el numero que queremos realizar la ID y el numero de dígitos de esa ID.
+// Poscondición: guarda en id una cadena de caracteres, por ejemplo, si num = 12 y numDigitos = 4, en id se encontrará 0012
 void GenerarID(char *id, int num, int numDigitos);
 
 
 /* FUNCIONES NO EXPORTABLES */
 
-// Precondici�n: Debe haberse iniciado sesi�n o registrado con antelaci�n. Recibe la direcci�n de memoria del primer elemento del vector de estructuras tipo tPerfil
-//               y un entero que se corresponde con la posici�n del usuario que inicio sesi�n o se registr� con antelaci�n..
-// Poscondici�n: Menu de opcinoes que desembocar� en otro men� de configuraci�n dependiendo de las opciones elegidas por el administrador.
+// Precondición: Debe haberse iniciado sesión o registrado con antelación. Recibe la dirección de memoria del primer elemento del vector de estructuras tipo tPerfil
+//               y un entero que se corresponde con la posición del usuario que inicio sesión o se registró con antelación..
+// Poscondición: Menu de opcinoes que desembocará en otro menú de configuración dependiendo de las opciones elegidas por el administrador.
 static void MenuAdmin(tPerfil *infoper, int posUsua);
 
-// Precondici�n: Debe haberse iniciado sesi�n o registrado con antelaci�n. Recibe la direcci�n de memoria del primer elemento del vector de estructuras tipo tPerfil
-//               y un entero que se corresponde con la posici�n del usuario que inicio sesi�n o se registr� con antelaci�n..
-// Poscondici�n: Menu de opcinoes que desembocar� en otro men� de configuraci�n dependiendo de las opciones elegidas por el usuario.
+// Precondición: Debe haberse iniciado sesión o registrado con antelación. Recibe la dirección de memoria del primer elemento del vector de estructuras tipo tPerfil
+//               y un entero que se corresponde con la posición del usuario que inicio sesión o se registró con antelación..
+// Poscondición: Menu de opcinoes que desembocará en otro menú de configuración dependiendo de las opciones elegidas por el usuario.
 static void MenuUser(tPerfil *infoper, int posUsua);
 
-// Precondici�n: Debe haberse iniciado sesi�n o registrado y haber pasado por MenuUser anteriormente. Recibe la direcci�n de memoria del primer elemento del vector de estructuras tipo tPerfil
-//               y un entero que se corresponde con la posici�n del usuario que inicio sesi�n o se registr� con antelaci�n..
-// Poscondici�n: Menu de opcinoes que desembocar� en diferentes opciones de configuraci�n.
+// Precondición: Debe haberse iniciado sesión o registrado y haber pasado por MenuUser anteriormente. Recibe la dirección de memoria del primer elemento del vector de estructuras tipo tPerfil
+//               y un entero que se corresponde con la posición del usuario que inicio sesión o se registró con antelación..
+// Poscondición: Menu de opcinoes que desembocará en diferentes opciones de configuración.
 static void Perfil(tPerfil *infoper, int posUsua);
 
-// Precondici�n: Debe haberse iniciado sesi�n o registrado y haber pasado por MenuAdmin anteriormente. Recibe la direcci�n de memoria del primer elemento del vector de estructuras tipo tPerfil
-//               y un entero que se corresponde con la posici�n del usuario que inicio sesi�n o se registr� con antelaci�n..
-// Poscondici�n: Menu de opcinoes que desembocar� en diferentes opciones de configuraci�n.
+// Precondición: Debe haberse iniciado sesión o registrado y haber pasado por MenuAdmin anteriormente. Recibe la dirección de memoria del primer elemento del vector de estructuras tipo tPerfil
+//               y un entero que se corresponde con la posición del usuario que inicio sesión o se registró con antelación..
+// Poscondición: Menu de opcinoes que desembocará en diferentes opciones de configuración.
 static void Usuarios(tPerfil *infoper);
 
-// Precondici�n: 
-// Poscondici�n: Imprime ESI-SHARE por pantalla.
+// Precondición: 
+// Poscondición: Imprime ESI-SHARE por pantalla.
 static void ImprimirESISHARE(void);
 
-// Precondici�n: Recibe un vector de estructuras tipo tPerfil, el cual, debe estar inicializado o no.
-// Poscondici�n: En caso de que en Usuarios.txt no se encuentre ningun usuario, esta funci�n reserva memoria para uno nuevo, en caso contrario, si se encuentran n usuarios,
-//               reservar� memoria para n+1.
+// Precondición: Recibe un vector de estructuras tipo tPerfil, el cual, debe estar inicializado o no.
+// Poscondición: En caso de que en Usuarios.txt no se encuentre ningun usuario, esta función reserva memoria para uno nuevo, en caso contrario, si se encuentran n usuarios,
+//               reservará memoria para n+1.
 static void ReservarNuevoPerfil(tPerfil *infoper);
 
-// Precondici�n: Recibe un vector de estructuras tipo tPerfil, el cual, deber�a tener un tama�o m�nimo, en caso de no tenerlo, llama a SignUp para crear un nuevo usuario.
-// Poscondici�n: carga toda la informaci�n de Usuarios.txt en el vector de estructuras de tipo tPefil.
+// Precondición: Recibe un vector de estructuras tipo tPerfil, el cual, deberá tener un tamaño mínimo, en caso de no tenerlo, llama a SignUp para crear un nuevo usuario.
+// Poscondición: carga toda la información de Usuarios.txt en el vector de estructuras de tipo tPefil.
 void CargarPerfiles(tPerfil *infoper);
 
-// Precondici�n: recibe una direcci�n de memoria a la primera posici�n del vector infoper, dos cadenas de caracteres y la direcci�n de memoria de la variable que almacenar� la posici�n del usuario
+// Precondición: recibe una dirección de memoria a la primera posición del vector infoper, dos cadenas de caracteres y la dirección de memoria de la variable que almacenará la posición del usuario
 //               del cual queremos ver si existe o no.
-// Poscondici�n: Devuelve 1 si se ha encontrado en el registro y 0 si no se ha encontrado
+// Poscondición: Devuelve 1 si se ha encontrado en el registro y 0 si no se ha encontrado
 static int ValidarLogin(tPerfil *infoper, char usuario[MAX_U], char contrasena[MAX_C], int *pos);
 
-// Precondici�n: recibe una direcci�n de memoria a la primera posici�n del vector infoper.
-// Poscondici�n: a�ade en el vector de estructuras tPerfil un nuevo usuario e imprime en Usuarios.txt los datos de ese nuevo usuario.
+// Precondición: recibe una dirección de memoria a la primera posición del vector infoper.
+// Poscondición: añade en el vector de estructuras tPerfil un nuevo usuario e imprime en Usuarios.txt los datos de ese nuevo usuario.
 static int SignUp(tPerfil *infoper);
 
-// Precondici�n: recibe una direcci�n de memoria a la primera posici�n del vector infoper.
-// Poscondici�n: elimina tanto en el registro tPerfil tanto como en el fichero Usuarios.txt el usuario seleccionado.
+// Precondición: recibe una dirección de memoria a la primera posición del vector infoper.
+// Poscondición: elimina tanto en el registro tPerfil tanto como en el fichero Usuarios.txt el usuario seleccionado.
 static void BajaUsuario(tPerfil *infoper);
 
-// Precondici�n: recibe una direcci�n de memoria a la primera posici�n del vector infoper.
-// Poscondici�n: Imprime por pantalla todos los usuarios con sus respectivos datos, y tambi�n el numero de usuarios registrados en el sistema.
+// Precondición: recibe una dirección de memoria a la primera posición del vector infoper.
+// Poscondición: Imprime por pantalla todos los usuarios con sus respectivos datos, y también el numero de usuarios registrados en el sistema.
 static void ListarPerfiles(tPerfil *infoper);
 
-// Precondici�n: recibe una direcci�n de memoria a la primera posici�n del vector infoper y la posici�n del usuario que incio sesi�n al principio del programa.
-// Poscondici�n: cambia tanto en el registro como en el fichero, los datos que puede cambiar un perfil de tipo usuario.
+// Precondición: recibe una dirección de memoria a la primera posición del vector infoper y la posición del usuario que incio sesión al principio del programa.
+// Poscondición: cambia tanto en el registro como en el fichero, los datos que puede cambiar un perfil de tipo usuario.
 static void ModificarCamposUsuario(tPerfil *infoper, int pos);
 
-// Precondici�n: recibe una direcci�n de memoria a la primera posici�n del vector infoper.
-// Poscondici�n: tras elegir qu� usuario cambiar datos, cambia tanto en el registro como en el fichero.
+// Precondición: recibe una dirección de memoria a la primera posición del vector infoper.
+// Poscondición: tras elegir qué usuario cambiar datos, cambia tanto en el registro como en el fichero.
 static void ModificarCamposAdmin(tPerfil *infoper);
 
-// Precondici�n: 
-// Poscondici�n: devuelve el numero de l�neas que tiene Usuarios.txt, que es a su vez el tama�o que corresponde al vector
+// Precondición: 
+// Poscondición: devuelve el numero de líneas que tiene Usuarios.txt, que es a su vez el tamaño que corresponde al vector
 static int LongitudVectorEstructuras(void);
 
-// Precondici�n: recibe una cadena de caracteres y el tama�o de dicha cadena.
-// Poscondici�n: limpia la cadena poniendo \0 en todas las posiciones de la cadena
+// Precondición: recibe una cadena de caracteres y el tamaño de dicha cadena.
+// Poscondición: limpia la cadena poniendo \0 en todas las posiciones de la cadena
 static void LimpiarCadena(char *cad, int tam);
 
-// Precondici�n: recibe un vector de estructuras tPerfil, una cadena de caracteres (ID), una direcci�n de memoria a la variable que contenga la posici�n del usuario
-//               y una variable que contenga el tama�o del vector de estructuras tPerfil.
-// Poscondici�n: Devuelve 1 si la ID es v�lida, es decir, si la ID se encuentra en el registro y 0 en caso contrario.
+// Precondición: recibe un vector de estructuras tPerfil, una cadena de caracteres (ID), una dirección de memoria a la variable que contenga la posición del usuario
+//               y una variable que contenga el tamaño del vector de estructuras tPerfil.
+// Poscondición: Devuelve 1 si la ID es válida, es decir, si la ID se encuentra en el registro y 0 en caso contrario.
 static int ValidarID(tPerfil *infoper, char id[ID], int *posUsua, int tam);
 
-// Precondici�n: recibe una cadena de caracteres, la cual no debe estar inicializada
-// Poscondici�n: guarda en la cadena el nombre de usuario que se introduzca, con varios mensajes de error en caso de que no sea v�lida.
+// Precondición: recibe una cadena de caracteres, la cual no debe estar inicializada
+// Poscondición: guarda en la cadena el nombre de usuario que se introduzca, con varios mensajes de error en caso de que no sea válida.
 static void ObtenerNombreUsuario(char *nomUsuario);
 
-// Precondici�n: recibe una cadena de caracteres, la cual no debe estar inicializada
-// Poscondici�n: guarda en la cadena la localidad que se introduzca, con varios mensajes de error en caso de que no sea v�lida.
+// Precondición: recibe una cadena de caracteres, la cual no debe estar inicializada
+// Poscondición: guarda en la cadena la localidad que se introduzca, con varios mensajes de error en caso de que no sea válida.
 static void ObtenerLocalidad(char *nomLocalidad);
 
-// Precondici�n: recibe un vector de estructuras tPerfil y una cadena de caracteres, la cual no debe estar inicializada
-// Poscondici�n: guarda en la cadena el usuario que se introduzca, con varios mensajes de error en caso de que no sea v�lida o se encuentre ya en el registro.
+// Precondición: recibe un vector de estructuras tPerfil y una cadena de caracteres, la cual no debe estar inicializada
+// Poscondición: guarda en la cadena el usuario que se introduzca, con varios mensajes de error en caso de que no sea válida o se encuentre ya en el registro.
 static void ObtenerUsuario(tPerfil *infoper, char *usuario);
 
-// Precondici�n: recibe una cadena de caracteres, la cual no debe estar inicializada
-// Poscondici�n: guarda en la cadena la contrase�a que se introduzca, con varios mensajes de error en caso de que no sea v�lida.
+// Precondición: recibe una cadena de caracteres, la cual no debe estar inicializada
+// Poscondición: guarda en la cadena la contraseña que se introduzca, con varios mensajes de error en caso de que no sea válida.
 static void ObtenerContrasena(char *contrasena);
 
-// Precondici�n: recibe un vector de estructuras tPerfil, una cadena de caracteres y el tama�o del vector de estructuras.
-// Poscondici�n: pregunta y la ID, con mensajes de error en caso que no sea valido
+// Precondición: recibe un vector de estructuras tPerfil, una cadena de caracteres y el tamaño del vector de estructuras.
+// Poscondición: pregunta y la ID, con mensajes de error en caso que no sea valido
 static void ObtenerID(tPerfil *infoper, char *id, int tam);
 
-// Precondici�n: recibe una cadena de caracteres.
-// Poscondici�n: pregunta y obtiene el usuario, con mensajes de error en caso que no sea valido
+// Precondición: recibe una cadena de caracteres.
+// Poscondición: pregunta y obtiene el usuario, con mensajes de error en caso que no sea valido
 static void PreguntarUsuario(char *user);
 
-// Precondici�n: recibe una cadena de caracteres.
-// Poscondici�n: pregunta y la ID, con mensajes de error en caso que no sea valido
+// Precondición: recibe una cadena de caracteres.
+// Poscondición: pregunta y la ID, con mensajes de error en caso que no sea valido
 static void CambiarID(char *id);
 
-// Precondici�n: 
-// Poscondici�n: Devuelve un char que contiene '1' pasa estar activo o '0' si pasa a estar bloqueados
+// Precondición: 
+// Poscondición: Devuelve un char que contiene '1' pasa estar activo o '0' si pasa a estar bloqueados
 static char ObtenerEstado(void);
 
 #endif
